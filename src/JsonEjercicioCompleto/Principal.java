06845d9570be0fc7a53537cbce3360b3e23e6dba
Liz Contreras <lizcontreras2224h@gmsil.com>
2024-10-11 07:33:29 +0200

    Estamos visualizando el fichero json de starwars

--- src/JsonEjercicioCompleto/Principal.java
+++ src/JsonEjercicioCompleto/Principal.java
@@ -1,31 +1,32 @@
 
-package JsonEjercicioCompleto;
-
+import JsonEjercicioCompleto.Especies;
+import JsonEjercicioCompleto.Peliculas;
+import JsonEjercicioCompleto.PersonajeAvanzado;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.IOException;
+import org.json.JSONException;
 
-/**
- *
- * @author FP
- */
 public class Principal {
+
     public static void main(String[] args) {
-         try {
-            //Leer el contenido desde un archivo JSON
-            String contenido = new String(java.nio.file.Files.readAllBytes(java.nio.file.Paths.get("starwars.json")));
+        try {
+            //Leer el contenido del archivo JSON
+            String contenido = new String(Files.readAllBytes(Paths.get("starwars.json")));
             JSONObject jsonLeido = new JSONObject(contenido);
 
-            //Creamos proyecto de participantes
-            PersonajeAvanzado proyecto = new PersonajeAvanzado("StarWars");
-
-            //Recuperear datos del proyecto
+            //Recuperar datos del proyecto
             String nombreProyecto = jsonLeido.getString("name");
-            String heightProyecto = jsonLeido.getString("height");
+            int heightProyecto = jsonLeido.getInt("height");  // Asegúrate de que es un número
             JSONArray jsonPeliculasLeidas = jsonLeido.getJSONArray("films");
 
-            //Creamos un proyecto con los datos leídos
+            //Objeto con los datos leídos
             PersonajeAvanzado proyectoLeido = new PersonajeAvanzado(nombreProyecto);
+            proyectoLeido.setHeight(heightProyecto);  // Asignar el height leído
 
+            //procesamos peliculas recorriendolas
             for (int i = 0; i < jsonPeliculasLeidas.length(); i++) {
                 JSONObject jsonStarwars = jsonPeliculasLeidas.getJSONObject(i);
                 String tituloPelicula = jsonStarwars.getString("title");
@@ -35,14 +36,27 @@ public class Principal {
 
                 Peliculas filLeido = new Peliculas(tituloPelicula, episodioId, opening_crawl, director);
 
-                //Agrego el ArrayList
+                //agurego las peliculas al proyecto
                 proyectoLeido.agregarPeliculas(filLeido);
-                
-                
             }
 
-        } catch (java.io.IOException e) {
+            //Imprimo el proyecto
+            System.out.println("name: " + proyectoLeido.getNombre());
+            System.out.println("height: " + proyectoLeido.getHeight());
+            System.out.println("films:");
+
+            //Imprimo peliculas
+            for (Peliculas pelicula : proyectoLeido.getPeliculas()) {
+                System.out.println("Title: " + pelicula.getTittle()
+                        + ",\n Episode_id: " + pelicula.getEpisodio()
+                        + ",\n Director: " + pelicula.getDirector());
+            }
+
+          
+        } catch (IOException e) {
             e.printStackTrace();
+        } catch (JSONException e) {
+            System.out.println("Error al procesar el JSON: " + e.getMessage());
         }
     }
 }
